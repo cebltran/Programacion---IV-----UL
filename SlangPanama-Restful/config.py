1a53b83f39667404fec5eb2b54435b8168d6942e
cebltran <74554890+cebltran@users.noreply.github.com>
2021-04-30 11:53:21 -0500

    Sustituir Base de Datos por REST API
     Sustituir Base de Datos por REST API

---
+++ SlangPanama-Restful/config.py
@@ -0,0 +1,15 @@
+import  ssl, smtplib
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+
+def eviocorreo ( destino, texto):
+    context = ssl.create_default_context()
+    usuario = 'parcialmodulo8@gmail.com'
+    contras = 'modulo2021'
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(usuario, contras)
+        server.sendmail(usuario, destino, texto)
+
+
